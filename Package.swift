e0443d23d6aa589f80008944b91942f728bc9c8c
Võ Hữu Toàn <vohuutoan@vnpt.vn>
2023-01-11 16:38:09 +0700

    fix Package.swift

--- Package.swift
+++ Package.swift
@@ -19,10 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Sources",
-            dependencies: []),
+            name: "projections-ios",
+            dependencies: [],
+            path:"Sources"),
         .testTarget(
-            name: "Tests",
-            dependencies: ["projections-ios"]),
+            name: "projections-iosTests",
+            dependencies: ["projections-ios"],
+            path:"Tests"),
     ]
 )
